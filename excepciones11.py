6b32c326bda3a801e51fb7ee3f461b5cecffdde1
Cesar Hera <cesar.hr2003@gmail.com>
2021-01-25 21:35:24 -0600

    Agrego aporte en programa de excepciones

--- excepciones11.py
+++ excepciones11.py
@@ -11,5 +11,13 @@ def run():
 
     print(dividir_lista(lista, divisor))
 
+# Excepciones comunes:
+# ImportError : una importación falla;
+# IndexError : una lista se indexa con un número fuera de rango;
+# NameError : se usa una variable desconocida ;
+# SyntaxError : el código no se puede analizar correctamente
+# TypeError : se llama a una función en un valor de un tipo inapropiado;
+# ValueError : se llama a una función en un valor del tipo correcto, pero con un valor inapropiado
+
 if __name__ == '__main__':
     run()
